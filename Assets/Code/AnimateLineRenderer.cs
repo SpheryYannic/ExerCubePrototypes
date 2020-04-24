ed619850552ae87435ca76a0de1600e1508b5a22
Yannic <info@huya.ch>
2020-04-24 16:39:23 +0200

    Skybox

---
+++ Assets/Code/AnimateLineRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateLineRenderer : MonoBehaviour
+{
+    LineRenderer rend;
+    public Color col;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<LineRenderer>();
+        rend.material.SetColor("_EmissionColor", col);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var currOffset = rend.material.GetTextureOffset("_MainTex");
+        rend.material.SetTextureOffset("_MainTex", new Vector2((currOffset.x + Time.deltaTime/10), currOffset.y));
+    }
+}
---
+++ Assets/Code/DestroyWhenTouched.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenTouched : MonoBehaviour
+{
+    public float minDistance;
+    BoxCollider coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<BoxCollider>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Tracker") && other.transform.localScale.x > coll.transform.localScale.x)
+        {
+            var dis = Vector3.Distance(other.transform.position, transform.position);
+
+            if (dis < minDistance)
+            {
+                Debug.Log("Entered");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
